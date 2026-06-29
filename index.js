27b1ba17ebb45d580dd95552e9942be494316777
Daniella Achoki <daniellaachoki@gmail.com>
2026-06-29 15:59:52 +0300

    Complete functions lab: five budgeting functions

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-
-
-
+function calculateTax(amount){
+    return amount*0.1;
+}
+function convertToUpperCase(text){
+    return text.toUpperCase();
+}
+function findMaximum(num1, num2){
+    return Math.max(num1,num2);
+}
+function isPalindrome(word){
+    return word=== word.split("").reverse().join("");
+}
+function calculateDiscountedPrice(originalPrice, discountPercentage){
+    return originalPrice - originalPrice * (discountPercentage/100);
+}
 
 
 // This is required for the test to function properly  
